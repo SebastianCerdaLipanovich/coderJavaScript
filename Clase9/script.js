70be8ab504e1cad469b5f3e5efc0ef0a1d5798ea
Sebastian Cerda <scerdalipanovich@gmail.com>
2021-11-04 23:39:37 -0300

    Ultimas validaciones

--- Clase9/script.js
+++ Clase9/script.js
@@ -22,7 +22,7 @@ class Alumno {
             } else {
                 let sumaNotas = 0;
                 this.notas.forEach(examen => {
-                    sumaNotas += examen.nota;
+                    sumaNotas += parseInt(examen.nota);
                 });
                 return Math.round((sumaNotas / this.notas.length + Number.EPSILON) * 100) / 100;
             }
@@ -65,15 +65,6 @@ class Examen {
  *          FUNCIONES
  ******************************************************/
 
-function pedirNota(materia) {
-    let ingreso = prompt(`Cual fue la nota del examen de ${materia}?`);
-    while (!esValido(ingreso)) {
-        alert("INGRESO NO VALIDO \nIntente de nuevo");
-        ingreso = prompt(`Cual fue la nota del examen de ${materia}?`);
-    }
-    ingreso = parseFloat(ingreso);
-    return Math.round((ingreso + Number.EPSILON) * 100) / 100;
-};
 function esContinuar(ingreso) {
     while (ingreso != "N" && ingreso != "Y") {
         alert("INGRESO NO VALIDO \nIntente de nuevo");
@@ -86,7 +77,7 @@ function esContinuar(ingreso) {
     }
 };
 function esValido(ingreso) {
-    return !(isNaN(parseFloat(ingreso)) || parseFloat(ingreso) > 10 || parseFloat(ingreso) < 0);
+    return (ingreso == 0 || ingreso == 1 || ingreso == 2 || ingreso == 3 || ingreso == 4 || ingreso == 5 || ingreso == 6 || ingreso == 7 || ingreso == 8 || ingreso == 9 || ingreso == 10);
 };
 
 /******************************************************
@@ -109,7 +100,7 @@ if ((localStorage.getItem('alumno')) && (document.getElementById("btnCrearAlumno
     document.getElementById("carreraInput").setAttribute("disabled", true);
     document.getElementById("mensajeLogin").innerHTML = `Continua ingresando examenes para este alumno o <a id="cierraSesion" href="#">cierra sesion</a>`
     document.getElementById("btnCrearAlumno").innerHTML = "Ingresar Examen"
-    document.getElementById("btnCrearAlumno").setAttribute("type","button");
+    document.getElementById("btnCrearAlumno").setAttribute("type", "button");
     document.getElementById("cierraSesion").onclick = () => {
         localStorage.removeItem("alumno");
         location.reload();
@@ -123,9 +114,14 @@ if (document.getElementById("btnCrearAlumno")) {
         let carrera = document.getElementById("carreraInput").value.toUpperCase();
 
         if (nombre && apellido && carrera) {
-            localStorage.setItem('alumno', JSON.stringify(new Alumno(nombre, apellido, carrera)));
-            document.getElementById("btnCrearAlumno").setAttribute("type","button");
-            location.href = "../Clase9/views/examenes.html";
+            if (localStorage.getItem('alumno')) {
+                document.getElementById("btnCrearAlumno").setAttribute("type", "button");
+                location.href = "../Clase9/views/examenes.html";
+            } else {
+                localStorage.setItem('alumno', JSON.stringify(new Alumno(nombre, apellido, carrera)));
+                document.getElementById("btnCrearAlumno").setAttribute("type", "button");
+                location.href = "../Clase9/views/examenes.html";
+            }
         };
     };
 };
@@ -141,7 +137,7 @@ if (document.getElementById("btnCrearExamen")) {
         let nota = document.getElementById("notaInput").value;
         if (localStorage.getItem('alumno')) {
             let alumno = new Alumno(JSON.parse(localStorage.getItem('alumno')).nombre, JSON.parse(localStorage.getItem('alumno')).apellido, JSON.parse(localStorage.getItem('alumno')).carrera, JSON.parse(localStorage.getItem('alumno')).notas);
-            if (esValido(nota)) {
+            if (esValido(nota) && materia) {
                 alumno.agregarNota(new Examen(materia, nota));
                 localStorage.setItem('alumno', JSON.stringify(alumno));
             };
@@ -165,82 +161,19 @@ if (document.getElementById("examenesTable")) {
         };
         examenesTable.appendChild(tr);
     };
+    if (alumno.notas.length > 0) {
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
+        let tdText = document.createTextNode(`PROMEDIO GENERAL`);
+        td.appendChild(tdText);
+        tr.appendChild(td);
+        examenesTable.appendChild(tr);
+        td = document.createElement("td");
+        tdText = document.createTextNode(`${alumno.sacarPromedioGeneral()}`);
+        td.appendChild(tdText);
+        tr.appendChild(td);
+        examenesTable.appendChild(tr);
+    };
 
 };
 
-function genera_tabla() {
-    // Obtener la referencia del elemento body
-    var body = document.getElementsByTagName("body")[0];
-
-    // Crea un elemento <table> y un elemento <tbody>
-    var tabla = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-
-    // Crea las celdas
-    for (var i = 0; i < 2; i++) {
-        // Crea las hileras de la tabla
-        var hilera = document.createElement("tr");
-
-        for (var j = 0; j < 2; j++) {
-            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-            // texto sea el contenido de <td>, ubica el elemento <td> al final
-            // de la hilera de la tabla
-            var celda = document.createElement("td");
-            var textoCelda = document.createTextNode("celda en la hilera " + i + ", columna " + j);
-            celda.appendChild(textoCelda);
-            hilera.appendChild(celda);
-        }
-
-        // agrega la hilera al final de la tabla (al final del elemento tblbody)
-        tblBody.appendChild(hilera);
-    }
-
-    // posiciona el <tbody> debajo del elemento <table>
-    tabla.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tabla);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
-    tabla.setAttribute("border", "2");
-}
-/* let alumno;
-if (localStorage.getItem('alumno')) {
-    let storageAlumno = localStorage.getItem('alumno');
-    alumno = new Alumno(JSON.parse(storageAlumno).nombre, JSON.parse(storageAlumno).apellido, JSON.parse(storageAlumno).carrera, JSON.parse(storageAlumno).notas);
-} else {
-    document.getElementById(nombreInput);
-    alumno = crearAlumno();
-    localStorage.setItem('alumno', JSON.stringify(alumno));
-}
-alert(`Hola ${alumno.nombre}, voy a pedirte que vayas ingresando todas tus notas aclarando materia y nota`);
-alumno.agregarNota(crearExamen());
-localStorage.setItem('alumno', JSON.stringify(alumno));
-let continuar = esContinuar(prompt("Deseas continuar? Y/N").toUpperCase());
-while (continuar) {
-    alumno.agregarNota(crearExamen());
-    localStorage.setItem('alumno', JSON.stringify(alumno));
-    continuar = esContinuar(prompt("Deseas continuar? Y/N").toUpperCase());
-};
-let h2 = document.createElement('h2');
-h2.innerHTML = `${alumno.nombre} ${alumno.apellido}`
-document.body.appendChild(h2);
-let h3 = document.createElement('h3');
-h3.innerHTML = `${alumno.carrera}`
-document.body.appendChild(h3);
-for (const examen of alumno.notas) {
-    let div = document.createElement("div");
-    div.innerHTML = `<h2>${examen.materia}</h2>
-                     <p class="nota">${examen.nota}</p>
-                     <hr>`;
-    document.body.appendChild(div);
-}
-let div = document.createElement("div");
-div.innerHTML = `<h2>PROMEDIO GENERAL</h2>
-                 <p>${alumno.sacarPromedioGeneral()}</p>
-                 <hr>`;
-document.body.appendChild(div);
-notas = document.querySelectorAll(".nota");
-notas.forEach(nota => {
-    if (parseFloat(nota.innerHTM) < 6) {
-        nota.style.color = "red";
-    }
-}); */
